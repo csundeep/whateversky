5f5691f0c3888527261521d903feedb7877a0488
sandychowdary.535 <sandychowdary.535@gmail.com>
2017-07-20 16:21:34 -0500

    Changed the base URI

--- rest/clearsky/src/main/java/com/clearsky/WebInitializer.java
+++ rest/clearsky/src/main/java/com/clearsky/WebInitializer.java
@@ -17,6 +17,6 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[] { "/v1/api/*" };
+		return new String[] { "/api/*" };
 	}
 }
